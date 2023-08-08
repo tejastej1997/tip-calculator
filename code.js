bd36ed583e31a1fb17ecab5db504f82b8c88abe7
tejastej1997 <tejastej1997@gmail.com>
2023-08-08 21:26:24 +0530

    java script functionality added for input tag

--- code.js
+++ code.js
@@ -1,7 +1,18 @@
 
 let custombtn = document.getElementById('custom-btn')
 let custominput = document.getElementById('custom-input')
+let reset = document.getElementById('reset')
+
+let input = document.querySelectorAll('input')
 
 custombtn.addEventListener('click',()=>{
     custominput.classList.remove('hide')
+})
+
+reset.addEventListener('click',()=>{
+
+    for(let inputs of input){
+        inputs.value='';
+    }
+    console.log('reset triggered');
 })
