18cf22de8a94d41afbd458bc1026350c260c47b6
tejastej1997 <tejastej1997@gmail.com>
2023-08-08 20:48:09 +0530

    added with java script functionality

---
+++ code.js
@@ -0,0 +1,7 @@
+
+let custombtn = document.getElementById('custom-btn')
+let custominput = document.getElementById('custom-input')
+
+custombtn.addEventListener('click',()=>{
+    custominput.classList.remove('hide')
+})
