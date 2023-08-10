03c0006346a98442ba4b3d30c350c067b95f727f
tejastej1997 <tejastej1997@gmail.com>
2023-08-10 22:03:14 +0530

    functionality added for tip ammount

--- code.js
+++ code.js
@@ -7,6 +7,10 @@ let selectedtip = document.getElementsByClassName('tipcalculator__selected-tip')
 let enterbutton = document.getElementById('enter-button')
 let billinput = document.getElementById('bill-input')
 let billammount = document.getElementById('bill-ammount')
+let perperson = document.getElementById('per-person')
+
+let tipvalue ;
+
 
 console.log(tippercentage.length);
 
@@ -31,7 +35,8 @@ reset.addEventListener('click',()=>{
 for (let btn =0; btn<=tippercentage.length-2;btn++ ) {
 
     tippercentage[btn].addEventListener('click',()=>{
-        tipammount.innerHTML = tippercentage[btn].value;
+        tipvalue=tippercentage[btn].value;
+        tipammount.innerHTML = tippercentage[btn].value + "%";
         selectedtip[0].classList.remove('hide')
         // console.log(btn.value);
     })
@@ -49,12 +54,19 @@ enterbutton.addEventListener('click',()=>{
 
     if (billammount.value== '' || billammount.value== undefined) {
         billinput.classList.remove('hide')
+
+        perperson.innerHTML = "0.00"
        
     }
     else{
         billinput.classList.add('hide')
     }
 
+    perperson.innerHTML = ( Number(billammount.value)  * Number(tipvalue)) / 100;
+
+    console.log(typeof billammount.value);
+    console.log(typeof tippercentage.value);
+    console.log(tipvalue);
     
 
     
